abba8636ae42864771da2dfc74b29546c57beedf
hongchii <flyh0321@gmail.com>
2021-12-03 23:04:53 +0900

    질문 api 구현중
    날짜에 해당하는 질문 데이터 불러오는 기능 구현중입니다.

--- src/main/java/com/samyo/service/QuestionServiceImpl.java
+++ src/main/java/com/samyo/service/QuestionServiceImpl.java
@@ -17,7 +17,6 @@ public class QuestionServiceImpl implements QuestionService {
 	public QuestionVO getQuestion(int question_num) throws Exception{
 
 		QuestionMapper questionMapper = sqlSession.getMapper(QuestionMapper.class);
-
 		QuestionVO question = questionMapper.getQuestion(question_num);
 
 		return question;
--- src/main/java/com/samyo/web/QuestionController.java
+++ src/main/java/com/samyo/web/QuestionController.java
@@ -1,7 +1,9 @@
 package com.samyo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.samyo.domain.QuestionVO;
@@ -13,23 +15,15 @@ public class QuestionController {
 	@Autowired
 	private QuestionService questionService;
 
-	@RequestMapping(value = "/question")
-	public QuestionVO getQuestion() throws Exception {
 
+	@RequestMapping(value = "/question/{question_num}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	public QuestionVO getQuestion(@PathVariable("question_num") int question_num) throws Exception {
+		System.out.println(question_num);
+		
 		QuestionVO question = new QuestionVO();
-		question.setQuestion_num(1);
-		question.setQuestion("새해첫날입니다.");
+		question = questionService.getQuestion(question_num);
 		
-		/*
-		int question_num = 1;
-
-		QuestionVO question = questionService.getQuestion(question_num);
-
-		System.out.println("question !!!!!!!!!!!!!!!! ");
-
-		 question.getQuestion_num(); question.getQuestion();
-		 */
-		
+		System.out.println(question);
 		return question;
 	}
 }
