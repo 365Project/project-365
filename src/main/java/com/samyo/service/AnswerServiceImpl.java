04f0b290207e66111a8743c2b583e9a93f9ca451
dh_lim_ <wawa4989@naver.com>
2021-12-04 20:07:08 +0900

    [내 일기장] 내 답변보기(read) 구현완료
    - db에 있는 데이터로 select 테스트 완료
    - member_num과 question_num을 조건으로 쿼리문 구현

--- src/main/java/com/samyo/service/AnswerServiceImpl.java
+++ src/main/java/com/samyo/service/AnswerServiceImpl.java
@@ -1,10 +1,13 @@
 package com.samyo.service;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.samyo.domain.AnswerVO;
+import com.samyo.domain.QuestionVO;
 import com.samyo.mapper.AnswerMapper;
 
 @Service("answerService")
@@ -21,5 +24,16 @@ public class AnswerServiceImpl implements AnswerService {
 		AnswerMapper answerMapper = sqlSession.getMapper(AnswerMapper.class);
 		answerMapper.insertAnswer(answer);
 		return 0;
+	}
+
+
+	@Override
+	public List<AnswerVO> ReadAnswer(int question_num) throws Exception {
+
+		AnswerMapper answerMapper = sqlSession.getMapper(AnswerMapper.class);
+		//answerMapper.selectAnswer(question_num);
+		
+		List<AnswerVO> result = answerMapper.selectAnswer(question_num);
+		return result;
 	}
 }
