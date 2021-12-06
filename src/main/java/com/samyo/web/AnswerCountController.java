3ffde1570a868cab1902a35cebee91526124bdb3
hongchii <flyh0321@gmail.com>
2021-12-06 20:44:46 +0900

    CrossOrigin추가
    프론트단과 연결을 위해 CrossOrigin추가

--- src/main/java/com/samyo/web/AnswerCountController.java
+++ src/main/java/com/samyo/web/AnswerCountController.java
@@ -4,12 +4,14 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.samyo.service.AnswerCountService;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class AnswerCountController {
 
