6be22bf43bc70452ae3f1062eb56868618f8388c
hongchii <flyh0321@gmail.com>
2021-12-09 22:32:15 +0900

    카카오 로그인 수정
    프론트에서 로그인 호출시 member_num과 token 정보 넘겨주도록 코드 수정
    기존 코드 삭제

--- src/main/java/com/samyo/mapper/MemberMapper.java
+++ src/main/java/com/samyo/mapper/MemberMapper.java
@@ -7,4 +7,6 @@ public interface MemberMapper {
 	public void insertMember(HashMap<String, Object> userInfo);
 
 	public int getMember(Object id);
+	
+	public int getMemberNum(Object id);
 }
--- src/main/java/com/samyo/web/LoginController.java
+++ src/main/java/com/samyo/web/LoginController.java
@@ -30,7 +30,7 @@ public class LoginController {
 	public String getKakaoAuthUrl(HttpServletRequest request) throws Exception {
 		String reqUrl = "https://kauth.kakao.com/oauth/authorize" + "?client_id=f6901986138e44bdb93305d1621fd37b"
 				+ "&redirect_uri=http://localhost:8080/login/oauth_kakao" + "&response_type=code";
-				// + "&redirect_uri=http://61.72.99.219:9130/login/oauth_kakao" + "&response_type=code";
+		// + "&redirect_uri=http://61.72.99.219:9130/login/oauth_kakao" + "&response_type=code";
 
 		return reqUrl;
 	}
@@ -45,14 +45,12 @@ public class LoginController {
 		HashMap<String, Object> userInfo = kakaoAPI.getUserInfo(access_Token);
 		System.out.println("login Controller : " + userInfo);
 
-		// List<MemberVO> list = new ArrayList<>();
-
 		userInfo.put("id", userInfo.get("id"));
 		userInfo.put("nickname", userInfo.get("nickname"));
 		userInfo.put("email", userInfo.get("email"));
 		userInfo.put("gender", userInfo.get("gender"));
 		userInfo.put("age", userInfo.get("age"));
-		// userInfo.put("token", remappingFunction)
+
 		System.out.println();
 
 		MemberVO member = new MemberVO();
@@ -63,21 +61,36 @@ public class LoginController {
 		HashMap<String, String> result = new HashMap<String, String>();
 		if (cnt > 0) {
 			System.out.println("이미 가입 된 회원 정보");
+
+			// 멤버번호 가져오기
+			int member_num = memberService.getMemberNum(userInfo.get("id"));
 			// 클라이언트의 아이디가 존재할 때 세션에 해당 아이디와 토큰 등록
 			if (userInfo.get("id") != null) {
 				session.setAttribute("userId", userInfo.get("id"));
 				session.setAttribute("access_Token", access_Token);
 			}
-			result.put("result", "login");
-			
+
+			String token = access_Token.substring(5, 15);
+
+			// result.put("result", "login");
+			result.put("member_num", String.valueOf(member_num));
+			result.put("token", token);
+
 			return result;
 		} else {
 			memberService.insertMember(userInfo);
+			int member_num = memberService.getMemberNum(userInfo.get("id"));
 			if (userInfo.get("id") != null) {
 				session.setAttribute("userId", userInfo.get("id"));
 				session.setAttribute("access_Token", access_Token);
 			}
-			result.put("result", "join");
+
+			String token = access_Token.substring(5, 15);
+
+			//result.put("result", "login");
+			result.put("member_num", String.valueOf(member_num));
+			result.put("token", token);
+
 			return result;
 		}
 
@@ -86,14 +99,14 @@ public class LoginController {
 	@RequestMapping(value = "/logout")
 	public HashMap<String, String> logout(HttpSession session) {
 		String access_Token = (String) session.getAttribute("access_Token");
-		
+
 		HashMap<String, String> result = new HashMap<String, String>();
 		if (access_Token != null && !"".equals(access_Token)) {
 			kakaoAPI.kakaoLogout(access_Token);
 			session.removeAttribute("access_Token");
 			session.removeAttribute("userId");
 			result.put("result", "success");
-			
+
 			return result;
 		} else {
 			System.out.println("access_Token is null");
