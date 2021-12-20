1f12c1707e48c3afc4ef014b8468c6f674710a6d
hongchii <flyh0321@gmail.com>
2021-12-20 22:51:27 +0900

    add 'BACKEND' folder

---
+++  BACKEND/project_365_/SamYO/src/main/java/com/samyo/mapper/MemberMapper.java
@@ -0,0 +1,12 @@
+package com.samyo.mapper;
+
+import java.util.HashMap;
+
+public interface MemberMapper {
+
+	public void insertMember(HashMap<String, Object> userInfo);
+
+	public int getMember(Object id);
+	
+	public int getMemberNum(Object id);
+}
